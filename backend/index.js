a811dc3b178ce48cc33ccfc0cc59eefa6f310ccc
skush-1 <kushsumit27@gmail.com>
2024-09-04 12:03:56 +0530

    fix cross orgin deployment

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use(cookieParser());
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:`${process.env.FRONTEND_URL}`,
     credentials:true
 };
 app.use(cors(corsOption)); 
