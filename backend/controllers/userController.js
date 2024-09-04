70011049a173bc1455b1f229bce2a43d3db2d573
skush-1 <kushsumit27@gmail.com>
2024-09-04 13:18:09 +0530

    fix typo in usercontroller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,7 +79,7 @@ export const login = async (req, res) => {
       .cookie("token", token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
         httpOnly: true,
-        samSite: "None",
+        sameSite: "None",
         secure: true,
       })
       .json({
