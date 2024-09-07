ff09e6a9296e7b06028823fa2538e282b7b78ce1
skush-1 <kushsumit27@gmail.com>
2024-09-07 13:46:01 +0530

    remove chat-time

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -16,9 +16,9 @@ const Message = ({message}) => {
                     <img alt="Tailwind CSS chat bubble component" src={message?.senderId === authUser?._id ? authUser?.profilePhoto  : selectedUser?.profilePhoto } />
                 </div>
             </div>
-            <div className="chat-header">
-                <time className="text-xs opacity-50 text-white">12:45</time>
-            </div>
+            {/* <div className="chat-header">
+                <time className="text-xs opacity-50 text-white">{new Date().getHours()}:{new Date().getMinutes()}</time>
+            </div> */}
             <div className={`chat-bubble ${message?.senderId !== authUser?._id ? 'bg-gray-200 text-black' : ''} `}>{message?.message}</div>
         </div>
     )
